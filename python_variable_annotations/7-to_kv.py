3749c83573f7d4498c35e4fa457d3dda0bc7c0c4
IvaneHo <10608@holbertonstudents.com>
2025-08-01 15:59:36 +0200

    black

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple with k and the square of v as a float."""
-    return (k, float(v ** 2))
+    return (k, float(v**2))
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,8 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies its float input by multiplier."""
+
     def multiplier_func(x: float) -> float:
         return x * multiplier
+
     return multiplier_func
