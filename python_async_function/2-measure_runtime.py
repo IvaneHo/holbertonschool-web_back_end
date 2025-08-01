653978d981e7c806610e1c5af49f279bda6eb011
IvaneHo <10608@holbertonstudents.com>
2025-08-01 16:36:28 +0200

    dzzz

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
+"""
+This module contains a method
+"""
 import time
 import asyncio
-
-wait_n = __import__("1-concurrent_coroutines").wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Measure average runtime per wait_n execution."""
-    start = time.time()
+    """
+    The said method
+    """
+    start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    total_time = time.time() - start
+    end = time.perf_counter()
+    total_time = end - start
     return total_time / n
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,7 +6,7 @@ task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn task_wait_random n times and return list of all delays in order of completion."""
+    """Spawn task_wait_random n times and rpletion."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
     for task in asyncio.as_completed(tasks):
