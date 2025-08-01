fb42e70feeefbe61cb9a8981d57da8ed682e710b
IvaneHo <10608@holbertonstudents.com>
2025-08-01 16:21:58 +0200

    efef

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+"""
+This module provides an async coroutine that wait and return a random delay
+"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay between 0 and max_delay (included) seconds and returns the delay."""
+    """
+    The said method
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
+"""
+A module that contains a method
+"""
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times with max_delay and return sorted list of all delays."""
-    delays = []
-    for coroutine in asyncio.as_completed([wait_random(max_delay) for _ in range(n)]):
-        delay = await coroutine
-        delays.append(delay)
+    """
+    The said method
+    """
+    delays: List[float] = []
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        i = 0
+        while i < len(delays) and delays[i] < delay:
+            i += 1
+        delays.insert(i, delay)
+
     return delays
