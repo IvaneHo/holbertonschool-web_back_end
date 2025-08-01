7b3bfd5110d8edad6afafaffee3c4ed08b4adfa5
IvaneHo <10608@holbertonstudents.com>
2025-08-01 16:29:05 +0200

    sddscdc

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,8 @@ A module that contains a method
 """
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import asyncio
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
