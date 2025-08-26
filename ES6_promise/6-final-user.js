dac294f60f5dc3c9622f23a9ad823bc7ddbe6c59
IvaneHo <10608@holbertonstudents.com>
2025-08-26 10:00:52 +0200

    ah

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,9 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((r) => ({
-      status: r.status,
-      value: r.status === 'fulfilled' ? r.value : r.reason,
-    }))
-  );
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    })));
 }
