2da050423ebd6fd1a18cb556448b5e1263bd8f76
IvaneHo <10608@holbertonstudents.com>
2025-09-03 09:21:12 +0200

    fff

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,8 +5,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const input = data.trim();
   process.stdout.write(`Your name is: ${input}\n`);
-});
-
-process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
+  process.stdin.end(); // Stop listening to stdin
 });
