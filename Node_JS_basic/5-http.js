d853057ac4e61a10390840ed59552e131594bac2
IvaneHo <10608@holbertonstudents.com>
2025-09-03 09:39:30 +0200

    plkokol,

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,19 +1,43 @@
-const { createServer } = require('node:http');
+const http = require('node:http');
 const countStudents = require('./3-read_file_async');
 
-const app = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((error) => {
-        res.end(`This is the list of our students\n${error.message}`);
-      });
+const databasePath = process.argv[2];
+
+const app = http.createServer(async (request, response) => {
+  response.setHeader('Content-Type', 'text/plain');
+
+  if (request.url === '/') {
+    response.statusCode = 200;
+    response.end('Hello Holberton School!');
+  } else if (request.url === '/students') {
+    response.statusCode = 200;
+    response.write('This is the list of our students\n');
+
+    
+    const originalLog = console.log;
+    let capturedOutput = '';
+
+    
+    console.log = (message) => {
+      capturedOutput += `${message}\n`;
+    };
+
+    try {
+      
+      
+      await countStudents(databasePath);
+
+      
+      response.end(capturedOutput);
+    } catch (error) {
+      response.end(error.message);
+    } finally {
+      
+      console.log = originalLog;
+    }
   } else {
-    res.end('Hello Holberton School!');
+    response.statusCode = 404;
+    response.end('Not found');
   }
 });
 
