3f7e5125d2a3a490ea56236a4b560018c8cf7017
IvaneHo <10608@holbertonstudents.com>
2025-09-03 08:43:03 +0200

    egegegeg

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
+process.stdin.setEncoding('utf-8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name.trim()}\n`);
+  }
 });
 
 process.stdin.on('end', () => {
