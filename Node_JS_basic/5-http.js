6abb969fbd86b9ede009009dfc0dfef9c22806fe
IvaneHo <10608@holbertonstudents.com>
2025-09-03 09:37:28 +0200

    ooioioi

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,60 +1,21 @@
-const http = require('http');
-const fs = require('fs');
+const { createServer } = require('node:http');
 const countStudents = require('./3-read_file_async');
 
-const database = process.argv[2];
-
-const app = http.createServer((req, res) => {
+const app = createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.statusCode = 200;
-    res.write('This is the list of our students\n');
-    countStudents(database)
-      .then(() => {
-        fs.readFile(database, 'utf8', (err, data) => {
-          if (err) {
-            res.end('Cannot load the database');
-            return;
-          }
-          const lines = data.split('\n').filter((line) => line.trim() !== '');
-          if (lines.length === 0) {
-            res.end('Number of students: 0');
-            return;
-          }
-          const students = lines.slice(1);
-          const fields = {};
-          let total = 0;
-          for (const line of students) {
-            if (line.trim() !== '') {
-              const parts = line.split(',');
-              if (parts.length >= 4) {
-                const firstname = parts[0].trim();
-                const field = parts[3].trim();
-                if (!fields[field]) fields[field] = [];
-                fields[field].push(firstname);
-                total += 1;
-              }
-            }
-          }
-          res.write(`Number of students: ${total}\n`);
-          for (const [field, names] of Object.entries(fields)) {
-            res.write(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`);
-          }
-          res.end();
-        });
+  if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
       })
-      .catch(() => {
-        res.end('Cannot load the database');
+      .catch((error) => {
+        res.end(`This is the list of our students\n${error.message}`);
       });
   } else {
-    res.statusCode = 404;
-    res.end('Not found');
+    res.end('Hello Holberton School!');
   }
 });
 
 app.listen(1245);
-
 module.exports = app;
