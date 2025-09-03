38519aa03d949dc495c09fd2baf0e4ea6ac50bb9
IvaneHo <10608@holbertonstudents.com>
2025-09-03 08:35:49 +0200

    dzdzd

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,13 @@
-// Gets input from user and writes it to stdout
-process.stdin.setEncoding('utf-8'); // set encoding to utf-8
+// 1-stdin.js
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read(); // read input from stdin
-  if (name) process.stdout.write(`Your name is: ${name}`);
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
 });
-/**
- * If stdout process in stream comes from the terminal, end the process
- * with a message
- */
-process.stdin.on('close', () => {
-  process.stdout.write('This important software is now closing\n');
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
